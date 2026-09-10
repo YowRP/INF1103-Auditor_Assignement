3eb16e1eb37194cfc13fa14334222715e7fa5484
YowRP <2604111@sit.singaporetech.edu.sg>
2026-09-10 21:50:46 +0800

    Invalid digit

--- Auditor.py
+++ Auditor.py
@@ -1,6 +1,11 @@
 inventory = 0
 while True:
     user_input = input("Please enter the stock quantity")
+
+    if not user_input.isdigit():
+        print("Invalid input. Please enter a valid number.")
+        continue
+    
     inventory += int(user_input)
     user_end = input("Do you want to add more stock? (yes/no): ")
     if user_end.lower() == "no" or user_end.lower() == "n":
