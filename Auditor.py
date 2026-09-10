79371d280019d52f4ae2567521aa9fc3a5789af3
YowRP <2604111@sit.singaporetech.edu.sg>
2026-09-10 21:54:29 +0800

    Business rules

--- Auditor.py
+++ Auditor.py
@@ -5,10 +5,20 @@ while True:
     if not user_input.isdigit():
         print("Invalid input. Please enter a valid number.")
         continue
+
+    elif int(user_input) < 0:
+        print("Invalid input. Please enter a non-negative number.")
+        continue
     
     inventory += int(user_input)
+
     user_end = input("Do you want to add more stock? (yes/no): ")
     if user_end.lower() == "no" or user_end.lower() == "n":
         break
     else:
         continue
+
+print ("Total stock quantity:", inventory)
+
+
+    
