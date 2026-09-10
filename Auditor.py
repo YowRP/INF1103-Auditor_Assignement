4c34a144710a5ef65c2577aee85038f9480bc40e
YowRP <2604111@sit.singaporetech.edu.sg>
2026-09-10 21:49:52 +0800

    basic loop

--- Auditor.py
+++ Auditor.py
@@ -1,12 +1,9 @@
-
+inventory = 0
 while True:
-    inventory = []
-    user_input = input("Enter the stock quantity")
-    inventory.append(user_input)
+    user_input = input("Please enter the stock quantity")
+    inventory += int(user_input)
     user_end = input("Do you want to add more stock? (yes/no): ")
     if user_end.lower() == "no" or user_end.lower() == "n":
         break
     else:
         continue
-
-print(inventory)
